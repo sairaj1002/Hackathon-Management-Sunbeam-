e8af7a05383ff5c6d59a24ddf49d58d47baf30d9
Sairaj Jadhav <113417633+sairaj1002@users.noreply.github.com>
2026-07-31 00:17:23 +0530

    Delete HackathonBackend directory

--- HackathonBackend/src/main/java/com/backend/config/AppConfig.java
+++
@@ -1,8 +0,0 @@
-package com.backend.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class AppConfig {
-
-}
--- HackathonBackend/src/main/java/com/backend/security/AuthService.java
+++
@@ -1,9 +0,0 @@
-package com.backend.security;
-
-import com.backend.dtos.common.request.*;
-import com.backend.dtos.common.response.*;
-
-public interface AuthService {
-    AuthResponse register(CreateUserRequest request);
-    AuthResponse login(LoginRequest request);
-}
